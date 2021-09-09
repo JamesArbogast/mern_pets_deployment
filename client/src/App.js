9b401a1492b0bbb70f03379c09b604a6178706c2
James Arbogast <jamesarbogast52@gmail.com>
2021-09-09 15:12:25 -0700

    footer almost complete

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import EditPet from "./views/EditPet";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Cat from '../src/Img/whitecat150.png'
 import Dog from '../src/Img/dog150.png'
+import Rabbit from '../src/Img/rabbit100.png'
 
 function App() {
   return (
@@ -35,6 +36,43 @@ function App() {
         <Redirect from="/" to="/pets" noThrow="true" />
         <NotFound default path="/notFound"/>
       </Router>
+      <ul class="nav justify-content-center bcolor1 p-2 mt-4 p-4 d-flex align-items-center">
+        <img src={Rabbit} class="h-1 w-1 me-4"/>
+        <ul class="list-unstyled ms-4">
+          <li class="nav-item">
+            <a class="nav-link text-dark" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-dark" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-dark" href="#">Home</a>
+          </li>
+        </ul>
+        <ul class="list-unstyled">
+          <li class="nav-item">
+            <a class="nav-link text-dark" href="#">Adopt</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-dark" href="#">Adopt</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-dark" href="#">Adopt</a>
+          </li>
+        </ul>
+        <ul class="list-unstyled me-4">
+          <li class="nav-item">
+            <a class="nav-link text-dark" href="#">Donate</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-dark" href="#">Donate</a>
+          </li>
+          <li class="nav-item me-4">
+            <a class="nav-link text-dark" href="#">Donate</a>
+          </li>
+        </ul>
+        <img src={Rabbit} class="h-1 w-1 ml-3"/>
+      </ul>
     </div>
   );
 }
