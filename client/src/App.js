9dc67bc74c508c4cb3779766b011c791e53325e2
James Arbogast <jamesarbogast52@gmail.com>
2021-09-09 14:54:10 -0700

    updated nav with images

--- client/src/App.js
+++ client/src/App.js
@@ -7,27 +7,26 @@ import Pets from "./views/Pets";
 import Pet from "./views/Pet";
 import NewPet from "./views/NewPet";
 import EditPet from "./views/EditPet";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Cat from '../src/Img/whitecat150.png'
+import Dog from '../src/Img/dog150.png'
 
 function App() {
   return (
     <div>
-      <nav className="nav">
-        <h1 className="navbar-brand mb-0" style={{marginLeft: "90px"}}>Pet Shelter</h1>
-        <div style={{marginLeft: "90px"}}>
-          <Link
-            to="/pets"
-            style={{margin: "10px", textDecoration: "none", fontSize: "20px", color: "darkblue"}}
-          >
-            Home
-          </Link>
-          <Link
-            to="/pets/new"
-            style={{margin: "10px", textDecoration: "none", fontSize: "20px", color: "darkblue"}}
-          >
-            Add a pet to the shelter!
-          </Link>
-        </div>
-      </nav>
+      <ul class="nav justify-content-center bcolor1 p-2 mb-3 d-flex align-items-center">
+        <img src={Cat} class="h-1 w-1"/>
+        <li class="nav-item h4">
+          <a class="nav-link text-dark" href="#">Home</a>
+        </li>
+        <li class="nav-item h4">
+          <a class="nav-link text-dark" href="#">Adopt</a>
+        </li>
+        <li class="nav-item h4">
+          <a class="nav-link text-dark" href="#">Donate</a>
+        </li>
+        <img src={Dog} class="h-1 w-1"/>
+      </ul>
       <Router>
         <Pets path="/pets" />
         <Pet path="/pets/:id" />
