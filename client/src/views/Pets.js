f6a842cc2a02422119041fea439d0377aef81460
Myles Williams <83609054+MylesFW@users.noreply.github.com>
2021-09-10 18:32:06 -0700

    Home Page commit

--- client/src/views/Pets.js
+++ client/src/views/Pets.js
@@ -1,68 +1,100 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { Link, navigate } from "@reach/router";
-import "../App.css"
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link, navigate } from '@reach/router';
+import '../App.css';
 
-const Pets = (props) => {
-    const [pets, setPets] = useState([]);
+const Home = (props) => {
+	const [pets, setPets] = useState([]);
 
-    useEffect(() => {
-        axios
-        .get("http://localhost:5000/api/pets")
-        .then((res) => {
-            const sortedPets = res.data.sort((a, b) =>
-            a.type.localeCompare(b.type)
-            );
-            setPets(sortedPets);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-    }, []);
+	useEffect(() => {
+		axios
+			.get('http://localhost:5000/api/pets')
+			.then((res) => {
+				const sortedPets = res.data.sort((a, b) =>
+					a.type.localeCompare(b.type)
+				);
+				setPets(sortedPets);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, []);
 
-    const handleDelete = (deletedId) => {
-        axios
-            .delete("http://localhost:5000/api/pets/" + deletedId)
-            .then((res) => {
-                // At this point it is deleted from DB but we need to cause a re-render to remove it from the page.
-                const filteredPets = pets.filter((pet) => {
-                return pet._id !== deletedId;
-                });
+	const handleDelete = (deletedId) => {
+		axios
+			.delete('http://localhost:5000/api/pets/' + deletedId)
+			.then((res) => {
+				// At this point it is deleted from DB but we need to cause a re-render to remove it from the page.
+				const filteredPets = pets.filter((pet) => {
+					return pet._id !== deletedId;
+				});
 
-                setPets(filteredPets);
-            })
-            .catch((err) => {
-                console.log(err);
-        });
-    };
+				setPets(filteredPets);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
 
-    return (
-        <div className={"table"}>
-        <h3 style={{marginLeft: "85px", fontSize: "30px"}}>These pets are looking for a shelter.</h3>
-        <table className={"innerTable"} cellPadding="0px" cellSpacing="0px">
-            <thead>
-                <th>Name</th>
-                <th>Type</th>
-                <th>Actions</th>
-            </thead>
-        {pets.map((pet) => {
-            return (
-                <tr
-                    key={pet._id}
-                >
-                    <td>
-                    <Link to={"/pets/" + pet._id} style={{margin: "10px", textDecoration: "none", fontSize: "20px", color: "darkblue"}}>{pet.name}</Link>
-                    </td>
-                    <td>{pet.type}</td>
-                    <td>
-                        <Link to={`/pets/${pet._id}`} style={{margin: "10px", textDecoration: "none", fontSize: "20px", color: "darkblue"}}>details</Link> | <Link to={`/pets/${pet._id}/edit`} style={{margin: "10px", textDecoration: "none", fontSize: "20px", color: "darkblue"}}>edit</Link>
-                    </td>
-                </tr>
-            );
-        })}
-        </table>
-        </div>
-    );
+	return (
+		<div className={'table'}>
+			<h3 style={{ marginLeft: '85px', fontSize: '30px' }}>
+				These pets are looking for a shelter.
+			</h3>
+			<table className={'innerTable'} cellPadding="0px" cellSpacing="0px">
+				<thead>
+					<th>Name</th>
+					<th>Type</th>
+					<th>Actions</th>
+				</thead>
+				{pets.map((pet) => {
+					return (
+						<tr key={pet._id}>
+							<td>
+								<Link
+									to={'/pets/' + pet._id}
+									style={{
+										margin: '10px',
+										textDecoration: 'none',
+										fontSize: '20px',
+										color: 'darkblue',
+									}}
+								>
+									{pet.name}
+								</Link>
+							</td>
+							<td>{pet.type}</td>
+							<td>
+								<Link
+									to={`/pets/${pet._id}`}
+									style={{
+										margin: '10px',
+										textDecoration: 'none',
+										fontSize: '20px',
+										color: 'darkblue',
+									}}
+								>
+									details
+								</Link>{' '}
+								|{' '}
+								<Link
+									to={`/pets/${pet._id}/edit`}
+									style={{
+										margin: '10px',
+										textDecoration: 'none',
+										fontSize: '20px',
+										color: 'darkblue',
+									}}
+								>
+									edit
+								</Link>
+							</td>
+						</tr>
+					);
+				})}
+			</table>
+		</div>
+	);
 };
 
-export default Pets;
+export default Home;
