72fb87b7091163e63cf7fbb93f9e3afd2549e0ea
Myles Williams <83609054+MylesFW@users.noreply.github.com>
2021-09-22 15:31:28 -0700

    model update

--- server/models/pets.models.js
+++ server/models/pets.models.js
@@ -1,42 +1,62 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 PetSchema = new mongoose.Schema(
-    {
-        name : {
-            type: String,
-            required: [true, "{PATH} is required."],
-            minlength: [3, "{PATH} must be at least {MINLENGTH} characters."],
-        },
+	{
+		name: {
+			type: String,
+			required: [true, '{PATH} is required.'],
+			minlength: [3, '{PATH} must be at least {MINLENGTH} characters.'],
+		},
 
-        gender : {
-            type: String,
-            required: [true, "{PATH} is required."]
-        },
+		type: {
+			type: String,
+			required: [true, '{PATH} is required.'],
+			minlength: [3, '{PATH} must be at least {MINLENGTH} characters.'],
+		},
 
-        type: {
-            type: String,
-            required: [true, "{PATH} is required."],
-            minlength: [3, "{PATH} must be at least {MINLENGTH} characters."],
-        },
+		header: {
+			type: String,
+			required: [true, '{PATH} is required.'],
+			minlength: [3, '{PATH} must be at least {MINLENGTH} characters.'],
+			maxlength: [
+				120,
+				'{PATH} can not be at longer than {MINLENGTH} characters.',
+			],
+		},
 
-        fixed : {
-            type: Boolean,
-            required: [true, "{PATH} is required"]
-        },
+		description: {
+			type: String,
+			required: [true, '{PATH} is required.'],
+			minlength: [3, '{PATH} must be at least {MINLENGTH} characters.'],
+		},
 
-        description : {
-            type: String,
-            required: [true, "{PATH} is required."],
-            minlength: [3, "{PATH} must be at least {MINLENGTH} characters."],
-        },
+		src: {
+			type: String,
+			required: [true, '{PATH} is required.'],
+		},
 
-        likes: {
-            type: Number,
-            default: 0,
-        },
-    },
-        { timestamps: true }
+		// optional inputs
+
+		gender: {
+			type: String,
+		},
+
+		age: {
+			type: String,
+		},
+
+		fixed: {
+			type: Boolean,
+			default: false,
+		},
+
+		likes: {
+			type: Number,
+			default: 0,
+		},
+	},
+	{ timestamps: true }
 );
 
-const Pet = mongoose.model("pet", PetSchema);
+const Pet = mongoose.model('pet', PetSchema);
 module.exports = Pet;
